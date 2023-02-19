25be76822403f81d0e2aeadacfea26598359733e
Herald <98087876+Code-SHD@users.noreply.github.com>
2023-02-19 14:52:23 +0900

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 from tensorflow.keras.models import load_model
 import numpy as np
 
-model = load_model("E:\\Programming Projects\\Doing\\Stock AI\\Samsung Electron\\model\\2.hdf5")
+model = load_model("Your Model Path")
 
 _input = np.array([62900])
 
